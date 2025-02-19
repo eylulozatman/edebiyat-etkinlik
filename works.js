de4a0e43d922def65eaa3a7a3c6d18b239650029
Eylül Özatman <74192529+eylulozatman@users.noreply.github.com>
2025-02-19 13:44:51 +0300

    new features

--- works.js
+++ works.js
@@ -8,13 +8,38 @@ const periods = [
 
 const works = [
     { work: "İntibah", author: "Namık Kemal", period: "Tanzimat" },
+    { work: "Cezmi", author: "Namık Kemal", period: "Tanzimat" },
+    { work: "Şair Evlenmesi", author: "Şinasi", period: "Tanzimat" },
+    { work: "Araba Sevdası", author: "Recaizade Mahmut Ekrem", period: "Tanzimat" },
+    { work: "Şıpsevdi", author: "Hüseyin Rahmi Gürpınar", period: "Tanzimat" },
+    { work: "Felatun Bey ile Rakım Efendi", author: "Ahmet Mithat Efendi", period: "Tanzimat" },
+    
     { work: "Eylül", author: "Mehmet Rauf", period: "Servet-i Fünun" },
+    { work: "Mai ve Siyah", author: "Halit Ziya Uşaklıgil", period: "Servet-i Fünun" },
+    { work: "Aşk-ı Memnu", author: "Halit Ziya Uşaklıgil", period: "Servet-i Fünun" },
+    { work: "Kırık Hayatlar", author: "Halit Ziya Uşaklıgil", period: "Servet-i Fünun" },
     { work: "Sis", author: "Tevfik Fikret", period: "Servet-i Fünun" },
+    { work: "Rübab-ı Şikeste", author: "Tevfik Fikret", period: "Servet-i Fünun" },
+    
+    { work: "Ruh-ı Kemal", author: "Cenap Şahabettin", period: "Fecr-i Ati" },
+    { work: "Elhan-ı Şita", author: "Cenap Şahabettin", period: "Fecr-i Ati" },
+    { work: "Servet-i Fünun Beyannamesi", author: "Fecr-i Ati Topluluğu", period: "Fecr-i Ati" },
+    
     { work: "Bir Destan Gibi", author: "Nazım Hikmet", period: "Cumhuriyet Dönemi" },
-    { work: "Ateşten Gömlek", author: "Halide Edib Adıvar", period: "Milli Edebiyat" },
-    { work: "Şık", author: "Hüseyin Rahmi Gürpınar", period: "Tanzimat" },
-    { work: "Kiralık Konak", author: "Yakup Kadri Karaosmanoğlu", period: "Milli Edebiyat" },
     { work: "Kuyucaklı Yusuf", author: "Sabahattin Ali", period: "Cumhuriyet Dönemi" },
     { work: "Huzur", author: "Ahmet Hamdi Tanpınar", period: "Cumhuriyet Dönemi" },
-    { work: "Eğil Dağlar", author: "Yahya Kemal Beyatlı", period: "Milli Edebiyat" }
+    { work: "Saatleri Ayarlama Enstitüsü", author: "Ahmet Hamdi Tanpınar", period: "Cumhuriyet Dönemi" },
+    { work: "Ağrı Dağı Efsanesi", author: "Yaşar Kemal", period: "Cumhuriyet Dönemi" },
+    { work: "İnce Memed", author: "Yaşar Kemal", period: "Cumhuriyet Dönemi" },
+    { work: "Anayurt Oteli", author: "Yusuf Atılgan", period: "Cumhuriyet Dönemi" },
+    
+    { work: "Ateşten Gömlek", author: "Halide Edib Adıvar", period: "Milli Edebiyat" },
+    { work: "Vurun Kahpeye", author: "Halide Edib Adıvar", period: "Milli Edebiyat" },
+    { work: "Türkçülüğün Esasları", author: "Ziya Gökalp", period: "Milli Edebiyat" },
+    { work: "Eğil Dağlar", author: "Yahya Kemal Beyatlı", period: "Milli Edebiyat" },
+    { work: "Yaban", author: "Yakup Kadri Karaosmanoğlu", period: "Milli Edebiyat" },
+    { work: "Kiralık Konak", author: "Yakup Kadri Karaosmanoğlu", period: "Milli Edebiyat" },
+    { work: "Sodom ve Gomore", author: "Yakup Kadri Karaosmanoğlu", period: "Milli Edebiyat" },
+    { work: "Hikaye-i Sevda", author: "Ömer Seyfettin", period: "Milli Edebiyat" },
+    { work: "Bomba", author: "Ömer Seyfettin", period: "Milli Edebiyat" }
 ];
